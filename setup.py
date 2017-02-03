e4dfb42e21905613f95643e716943e67ef2f4256
Ganggao Zhu <zhuganggao@gmail.com>
2017-02-03 17:37:41 +0100

    upload datasets

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name = 'sematch',
+      packages=['sematch'],
+      version = '{ 1.1.0 }',
+      description = 'Semantic similarity framework for knowledge graphs',
+      author = 'Ganggao Zhu',
+      email = 'gzhu@dit.upm.es',
+      url = 'https://github.com/gsi-upm/sematch',
+      keywords = ['semantic similarity', 'taxonomy', 'knowledge graph', 
+      'semantic analysis', 'knowledge base', 'WordNet', 'DBpedia','YAGO', 'ontology'], 
+      classifiers = [], )
