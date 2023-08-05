52b5a1b0132ac27c950343e85d6e273c43673266
Ishika Johari <52175977+ishijo@users.noreply.github.com>
2023-08-05 11:06:05 +0530

    Update utility.py
    Since collections has been updated due to which the Hashable attribute is now present inside collections.abc, I changed "if not isinstance(args, collections.Hashable):" to "if not isinstance(args, collections.abc.Hashable):"

--- sematch/utility.py
+++ sematch/utility.py
@@ -69,7 +69,7 @@ class memoized(object):
       self.func = func
       self.cache = {}
    def __call__(self, *args):
-      if not isinstance(args, collections.Hashable):
+      if not isinstance(args, collections.abc.Hashable):
          # uncacheable. a list, for instance.
          # better to not cache than blow up.
          return self.func(*args)
